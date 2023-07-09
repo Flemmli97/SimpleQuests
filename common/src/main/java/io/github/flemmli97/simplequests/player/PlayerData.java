9d3054b87698c419eb900be6385f3ffede146a98
Flemmli97 <Flemmli97@users.noreply.github.com>
2023-07-09 17:35:57 +0200

    fix daily quest not ignored for max current quest

--- common/src/main/java/io/github/flemmli97/simplequests/player/PlayerData.java
+++ common/src/main/java/io/github/flemmli97/simplequests/player/PlayerData.java
@@ -68,7 +68,7 @@ public class PlayerData {
     public boolean acceptQuest(Quest quest) {
         int maxConcurrent = quest.category.getMaxConcurrentQuests();
         if (maxConcurrent > 0 && this.currentQuests.stream()
-                .filter(p -> !p.getQuest().isDailyQuest && !quest.category.sameCategoryOnly || p.getQuest().category == quest.category).toList().size() >= maxConcurrent) {
+                .filter(p -> !p.getQuest().isDailyQuest && (!quest.category.sameCategoryOnly || p.getQuest().category == quest.category)).toList().size() >= maxConcurrent) {
             this.player.sendMessage(new TranslatableComponent(ConfigHandler.lang.get("simplequests.active.full")).withStyle(ChatFormatting.DARK_RED), Util.NIL_UUID);
             return false;
         }
