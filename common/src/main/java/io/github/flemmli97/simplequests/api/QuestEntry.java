35d635a857730ec72936e55ae016271a3458e086
Flemmli97 <Flemmli97@users.noreply.github.com>
2024-03-04 16:07:35 +0100

    update to loom 1.4

--- common/src/main/java/io/github/flemmli97/simplequests/api/QuestEntry.java
+++ common/src/main/java/io/github/flemmli97/simplequests/api/QuestEntry.java
@@ -7,7 +7,7 @@ import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 
-import javax.annotation.Nullable;
+import org.jetbrains.annotations.Nullable;
 import java.util.function.Predicate;
 
 public interface QuestEntry {
