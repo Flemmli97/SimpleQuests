42808ea9337f50387338b2ef5d6f869af62256d2
Flemmli97 <Flemmli97@users.noreply.github.com>
2025-01-23 22:43:46 +0100

    fix data copy/save

--- common/src/main/java/io/github/flemmli97/simplequests/player/PlayerData.java
+++ common/src/main/java/io/github/flemmli97/simplequests/player/PlayerData.java
@@ -48,7 +48,7 @@ public class PlayerData {
     private final ServerPlayer player;
     private List<QuestProgress> currentQuests = new ArrayList<>();
     private Map<ResourceLocation, Long> cooldownTracker = new HashMap<>();
-    private final List<QuestProgress> tickables = new ArrayList<>();
+    private List<QuestProgress> tickables = new ArrayList<>();
 
     private Set<ResourceLocation> unlockTracker = new HashSet<>();
 
@@ -434,20 +434,22 @@ public class PlayerData {
         if (tag.contains("TimeTracker"))
             this.questTrackerTime = LocalDateTime.parse(tag.getString("TimeTracker"), TIME);
         CompoundTag daily = tag.getCompound("DailyQuestTracker");
-        daily.getAllKeys().forEach(key -> this.dailyQuestsTracker.put(ResourceLocation.parse(key), done.getInt(key)));
+        daily.getAllKeys().forEach(key -> this.dailyQuestsTracker.put(ResourceLocation.parse(key), daily.getInt(key)));
         CompoundTag dailyCategory = tag.getCompound("DailyQuestCategoryTracker");
-        dailyCategory.getAllKeys().forEach(key -> this.dailyQuestsCategoryTracker.put(ResourceLocation.parse(key), done.getInt(key)));
+        dailyCategory.getAllKeys().forEach(key -> this.dailyQuestsCategoryTracker.put(ResourceLocation.parse(key), dailyCategory.getInt(key)));
         CompoundTag total = tag.getCompound("FinishedQuestTracker");
-        total.getAllKeys().forEach(key -> this.finishedQuestsTracker.put(ResourceLocation.parse(key), done.getInt(key)));
+        total.getAllKeys().forEach(key -> this.finishedQuestsTracker.put(ResourceLocation.parse(key), total.getInt(key)));
         ListTag unlocked = tag.getList("UnlockedQuests", Tag.TAG_STRING);
         unlocked.forEach(t -> this.unlockTracker.add(ResourceLocation.parse(t.getAsString())));
     }
 
     public void clone(PlayerData data) {
         this.currentQuests = data.currentQuests;
+        this.tickables = data.tickables;
         this.cooldownTracker = data.cooldownTracker;
         this.unlockTracker = data.unlockTracker;
         this.questTrackerTime = data.questTrackerTime;
+        this.dailySeed = data.dailySeed;
         this.dailyQuestsTracker.clear();
         this.dailyQuestsTracker.putAll(data.dailyQuestsTracker);
         this.dailyQuestsCategoryTracker.clear();
@@ -459,6 +461,7 @@ public class PlayerData {
     public void resetAll() {
         this.currentQuests.forEach(p -> p.getQuest().onReset(this.player));
         this.currentQuests.clear();
+        this.tickables.clear();
         this.cooldownTracker.clear();
         this.unlockTracker.clear();
         this.questTrackerTime = null;
