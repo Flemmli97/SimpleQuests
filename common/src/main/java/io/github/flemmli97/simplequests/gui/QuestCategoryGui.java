ebb28529d34e58fe04d99460e831cb250f357fc7
Flemmli97 <Flemmli97@users.noreply.github.com>
2025-02-25 00:41:44 +0100

    fix gui sound missing

--- common/src/main/java/io/github/flemmli97/simplequests/gui/QuestCategoryGui.java
+++ common/src/main/java/io/github/flemmli97/simplequests/gui/QuestCategoryGui.java
@@ -164,6 +164,7 @@ public class QuestCategoryGui extends ServerOnlyScreenHandler<Object> {
         player.closeContainer();
         QuestGui.playSongToPlayer(player, SoundEvents.UI_BUTTON_CLICK, 1, 1f);
         player.getServer().execute(() -> QuestGui.openGui(player, category));
+        QuestGui.playSongToPlayer(player, SoundEvents.UI_BUTTON_CLICK, 1, 1f);
         return true;
     }
 
