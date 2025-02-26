5e683116c2a7d51a24d608c3be430417c189ea57
Flemmli97 <Flemmli97@users.noreply.github.com>
2025-02-26 12:50:50 +0100

    add admin mode that allows viewing of subquests

--- common/src/main/java/io/github/flemmli97/simplequests/data/PlayerData.java
+++ common/src/main/java/io/github/flemmli97/simplequests/data/PlayerData.java
@@ -64,6 +64,8 @@ public class PlayerData implements PlayerQuestData {
 
     private int interactionCooldown;
 
+    private boolean adminMode;
+
     public static PlayerData get(ServerPlayer player) {
         return ((SimpleQuestDataGet) player).simpleQuestPlayerData();
     }
@@ -235,7 +237,8 @@ public class PlayerData implements PlayerQuestData {
             return;
         } else if (forced || this.player.level.getGameTime() - this.resetTick < 600) {
             if (sendMsg)
-                this.player.sendMessage(new TranslatableComponent("simplequests.reset", prog.getName(this.player)).withStyle(ChatFormatting.DARK_RED), Util.NIL_UUID);
+                this.player.sendMessage(new TranslatableComponent("simplequests.reset", prog.getName(this.player)
+                        .withStyle(ChatFormatting.RED, ChatFormatting.BOLD)).withStyle(ChatFormatting.DARK_RED), Util.NIL_UUID);
             this.currentQuests.remove(prog);
             this.removeTickableQuestProgress(prog);
             prog.getQuest().onReset(this.player);
@@ -284,6 +287,14 @@ public class PlayerData implements PlayerQuestData {
     @Override
     public ServerPlayer getPlayer() {
         return this.player;
+    }
+
+    public boolean isAdminMode() {
+        return this.adminMode;
+    }
+
+    public void setAdminMode(boolean adminMode) {
+        this.adminMode = adminMode;
     }
 
     @Override
